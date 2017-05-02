e847df5556216bdf4dce7d8ed27c3ebb3cf9c79f
AleTorres <alejandro_torres_polo@hotmail.com>
2017-05-02 09:37:09 -0600

    Correction

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,9 +4,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if (answer = '' && attempt = '') {
-      setHiddenFields;
-    }
+
 }
 
 //implement new functions here
